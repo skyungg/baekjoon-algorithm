cbccf0b4bf0319f1a217328535f5ce6212c8423e
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-10 15:27:08 +0900

    [Bronze III] Title: 최소, 최대, Time: 504 ms, Memory: 88132 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -5,16 +5,17 @@ public class Main{
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long [] arr = new long[n];
+        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
-            arr[i] = Long.parseLong(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            
+            if(num < minNum) minNum = num;
+            if(num > maxNum) maxNum = num;
         }
-        
-        // 정렬
-        Arrays.sort(arr);
-        
-        System.out.println(arr[0]+" "+arr[n-1]);
+
+        System.out.println(minNum+" "+maxNum);
     }
 }

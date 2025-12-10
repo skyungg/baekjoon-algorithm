20d8c8fae4855015420c141e70e7da3005cefb04
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-10 15:17:53 +0900

    [Bronze III] Title: 최소, 최대, Time: 1200 ms, Memory: 108740 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long [] arr = new long[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        
+        // 정렬
+        Arrays.sort(arr);
+        
+        System.out.println(arr[0]+" "+arr[n-1]);
+    }
+}

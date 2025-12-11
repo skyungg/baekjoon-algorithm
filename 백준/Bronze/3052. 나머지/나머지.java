e7807fba667b504d73d52edc407c1c55c766fd56
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-11 20:34:36 +0900

    [Bronze II] Title: 나머지, Time: 96 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < 10; i++){
+            int n = Integer.parseInt(br.readLine());
+            
+            set.add(n%42);
+        }
+        
+        System.out.println(set.size());
+    }
+}

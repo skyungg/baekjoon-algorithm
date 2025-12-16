81cd14fe9ba3d8a9e21b62c16eb74a81baee3e0c
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-16 17:02:35 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = 0;    // 점수중 최댓값
+        int [] score = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            score[i] = Integer.parseInt(st.nextToken());
+            M = Math.max(M, score[i]);    // 최댓값 차기
+        }
+        
+        // 점수 조정하기
+        double sum = 0;
+        for(int i = 0; i < N; i++){
+            if(score[i] == M) sum += 100;
+            else{
+                sum += ((double)score[i]/M)*100;
+            }
+        }
+        
+        // 결과 출력
+        System.out.println(sum/N);
+    }
+}

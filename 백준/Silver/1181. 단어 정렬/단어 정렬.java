31e9a89b35407d09b9047dd7fdfd15d1614744b8
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-18 20:37:40 +0900

    [Silver V] Title: 단어 정렬, Time: 308 ms, Memory: 27620 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -8,15 +8,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		Set<String> set = new HashSet<>();;
+		String [] arr = new String[N];
 		
 		for(int i = 0; i < N; i++) {
-			set.add(br.readLine());
+			arr[i] = br.readLine();
 		}
 		
-		List<String> list = new ArrayList<>(set);
-		
-		Collections.sort(list, new Comparator<String>() {
+		Arrays.sort(arr, new Comparator<String>() {
 			@Override
 			public int compare(String s1, String s2) {
 				if(s1.length() == s2.length()) return s1.compareTo(s2);
@@ -26,7 +24,11 @@ public class Main {
 
 		
 		StringBuilder sb = new StringBuilder();
-		for(String str : list) sb.append(str+"\n");
+		sb.append(arr[0]+"\n");
+		
+		for(int i = 1; i < N; i++) {
+			if(!arr[i].equals(arr[i-1])) sb.append(arr[i]+"\n");
+		}
 		
 		System.out.println(sb);
 

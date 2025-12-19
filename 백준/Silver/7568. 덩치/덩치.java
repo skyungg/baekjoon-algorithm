ae26e2e7d1651786d03f7b7d9e79b93a249e2784
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-19 23:51:03 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int [][] arr = new int[N][2];
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		int [] result = new int[N];
+		for(int i = 0; i < N; i++) {
+			int count = 1;
+			for(int j = 0; j < N; j++) {
+				if(i == j) continue;
+				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) count++;
+			}
+			result[i] = count;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int n : result) sb.append(n+" ");
+		
+		System.out.println(sb);
+
+	}
+
+}

66a2cc85f711402081cbd29679b4e58237c492af
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-19 23:52:30 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -15,18 +15,15 @@ public class Main {
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		int [] result = new int[N];
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < N; i++) {
 			int count = 1;
 			for(int j = 0; j < N; j++) {
 				if(i == j) continue;
 				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) count++;
 			}
-			result[i] = count;
+			sb.append(count+" ");
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int n : result) sb.append(n+" ");
 		
 		System.out.println(sb);
 

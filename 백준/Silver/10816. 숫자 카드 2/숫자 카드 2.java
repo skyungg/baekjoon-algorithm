72715963794bbce5e844189bfc662e742372c711
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-23 18:37:39 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 836 ms, Memory: 214204 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -7,23 +7,20 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		HashMap<Integer, Integer> hmap = new HashMap<>();
+		int [] nums = new int[20000001];		// 음수인 값을 위하여 기존의 크기 2배하기
 		
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			int n = Integer.parseInt(st.nextToken());
-			
-			hmap.put(n, hmap.getOrDefault(n, 0)+1);
+			nums[n+10000000]++;		// 10000000더해서 인덱스 만들기
 		}
 		
 		StringBuilder sb = new StringBuilder();
 		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if(hmap.containsKey(num)) sb.append(hmap.get(num)+" ");
-			else sb.append("0 ");
+			sb.append(nums[Integer.parseInt(st.nextToken()) + 10000000]+" ");
 		}
 		
 		System.out.println(sb);

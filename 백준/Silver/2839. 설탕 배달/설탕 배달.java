d182150b96e37180f60eb99d61c4b5bc884c2773
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-23 17:01:54 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -12,19 +12,22 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		int count = 0;
-		boolean flag = false;
 		while(N >= 0) {
 			if(N % 5 == 0) {		// 5로 확실히 나누어 질 때만 5로 나누는게 깔끔.
 				count += N/5;
-				flag = true;
+				break;
+			}else {
+				N -= 3;			// 5로 나누어떨어지지 않으면 -3씩해서 간보기
+				count++;				
+			}
+			
+			if(N < 0) {
+				count = -1;
 				break;
 			}
-			N -= 3;			// 5로 나누어떨어지지 않으면 -3씩해서 간보기
-			count++;
 		}
 		
-		if(!flag) System.out.println(-1);
-		else System.out.println(count);
+		System.out.println(count);
 		
 		
 	}

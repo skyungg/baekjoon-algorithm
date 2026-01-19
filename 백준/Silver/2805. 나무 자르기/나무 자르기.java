a4d03ce9b45d6518c0af79d7dde3c1bb1de0c1a3
skyungg <167261107+skyungg@users.noreply.github.com>
2026-01-19 22:15:30 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 119440 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] trees;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        trees = new int[N];
+        int max = 0;
+        int min = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(trees[i],max);
+        }
+
+        //이분탐색
+        while(min<max){
+            int mid = (min+max)/2;
+            long sum = cutting(mid);
+
+            if(sum < M){
+                max = mid;
+            }else{
+                min = mid+1;
+            }
+        }
+        System.out.println(min-1);
+    }
+    static long cutting(int height){
+        long result = 0;
+        for(int i=0;i<trees.length;i++){
+            if(trees[i] > height){
+                result += trees[i] - height;
+            }
+        }
+        return  result;
+    }
+}

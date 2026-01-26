645f1c43320f9ce8b18c453c29b0c23317338091
skyungg <167261107+skyungg@users.noreply.github.com>
2026-01-26 22:05:31 +0900

    [Silver I] Title: 절댓값 힙, Time: 300 ms, Memory: 26496 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -8,12 +8,9 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		PriorityQueue<int []> pq = new PriorityQueue<>(new Comparator<int []>() {
-			@Override
-			public int compare(int [] a, int [] b) {
-				if(a[0] == b[0]) return a[1] - b[1];		// 절댓값 같은 경우 -> 원래 값 작은 순서로
-				else return a[0] - b[0];					// 기본: 절댓값 작은순서
-			}
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+			if(Math.abs(a) == Math.abs(b)) return a - b;		// 절댓값 같은 경우 -> 원래 값 작은 순서로
+			else return  Math.abs(a) - Math.abs(b);					// 기본: 절댓값 작은순서}
 		});
 
 		
@@ -23,9 +20,9 @@ public class Main {
 			
 			if(num == 0) {
 				if(pq.isEmpty()) sb.append(0+"\n");
-				else sb.append(pq.poll()[1]+"\n");
+				else sb.append(pq.poll()+"\n");
 			}else {
-				pq.add(new int [] {Math.abs(num), num});				
+				pq.add(num);				
 			}
 		}
 		

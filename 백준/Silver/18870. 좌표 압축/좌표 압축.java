eb0a942e2ef3912fa6a6f3d16e4f7b446dda4e82
skyungg <167261107+skyungg@users.noreply.github.com>
2026-01-30 22:21:28 +0900

    [Silver II] Title: 좌표 압축, Time: 1680 ms, Memory: 209532 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -8,27 +8,55 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int [] nums = new int[N];
+		int [] tmp = new int[N];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		Set<Integer> set = new HashSet<>();
 		for(int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
-			set.add(nums[i]);
+			tmp[i] = nums[i];
 		}
 		
-		List<Integer> list = new ArrayList<>(set);
-		Collections.sort(list);		// 오름차순 정렬
-		HashMap<Integer, Integer> hmap = new HashMap<>();
-		for(int i = 0; i < list.size(); i++) {
-			if(hmap.containsKey(list.get(i))) continue;
-			else hmap.put(list.get(i), i);
+		
+		// 오름차순 정렬
+		Arrays.sort(tmp);
+		int maxNum = tmp[0];
+		int count = 1;			// 중복 제거한 길이 
+		for(int i = 1; i < N; i++) {
+			if(tmp[i] > maxNum) {
+				maxNum = tmp[i];	// 배열 속 최댓값 갱신
+				count++;
+			}
 		}
 		
+		int [] result = new int[count];		// 중복 제거한 길이만큼 생성 -> 찐 중복제거한만큼 배열 만들기
+		result[0] = tmp[0];
+		maxNum = tmp[0];
+		count = 1;
+		for(int i = 1; i < N; i++) {
+			if(tmp[i] > maxNum) {
+				result[count++] = tmp[i];	// 중복제거한 값들을 저장
+				maxNum = tmp[i];
+			}
+		}
+		
+		// 기존 배열에서 좌표 압축 실행 -> 이분 탐색 통해 값 탐색
+		int left = 0;
+		int right = count-1;
+		int index = 0;
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < N; i++) {
-			sb.append(hmap.get(nums[i]) + " ");
+		while(index < N) {
+			int mid = (left+right)/2;
+			
+			if(result[mid] < nums[index]) left = mid+1;
+			else if(result[mid] > nums[index]) right = mid-1;
+			else {
+				left = 0;				// 포인터 초기화
+				right = count-1;
+				sb.append(mid+" ");
+				index++;
+			}
 		}
-		
+
 		// 출력
 		System.out.println(sb);
 	}

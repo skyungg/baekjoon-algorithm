7bb287f00c0299dd503b9b74f03f0e6a7bf031a0
skyungg <167261107+skyungg@users.noreply.github.com>
2026-02-06 12:06:02 +0900

    [Gold V] Title: 토마토, Time: 472 ms, Memory: 128124 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -7,7 +7,6 @@ public class Main {
 	static int [] dx = {-1, 0, 1, 0};
 	static int [] dy = {0, 1, 0, -1};
 	static Queue<int[]> que = new LinkedList<>();
-//	static boolean [][] visited;
 	static int count = 0;	// 익지 않은 토마토 개수
 	public static void main(String [] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,14 +16,12 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());	// 행
 		
 		map = new int[N][M];
-//		visited = new boolean[N][M];
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if(map[i][j] == 1) {
 					que.add(new int [] {i, j, 0});	// 토마토인 경우
-//					visited[i][j] = true;
 				}
 				if(map[i][j] == 0)count++;	// 익지 않은 토마토
 			}
@@ -52,7 +49,7 @@ public class Main {
 					if(map[tx][ty] == 0) {		// 익지 않은 토마토 + 아직 방문전
 						map[tx][ty] = 1;
 						que.add(new int [] {tx, ty, point[2]+1});
-						result = Math.max(result, point[2]+1);
+						result = point[2]+1;
 						count--;
 						}
 					}

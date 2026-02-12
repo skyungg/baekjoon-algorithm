3396d841b889e666847c18fa7793a5c7919a0f73
skyungg <167261107+skyungg@users.noreply.github.com>
2026-02-12 18:10:03 +0900

    [Gold V] Title: 적록색약, Time: 132 ms, Memory: 16616 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,9 +3,7 @@ import java.util.*;
 
 public class Main {
 	static int N;
-	static char [][] map;
 	static boolean [][] visited;
-	static boolean [][] rgVisited;
 	static int [] dx = {-1, 0, 1, 0};
 	static int [] dy = {0, 1, 0, -1};
 	
@@ -13,12 +11,17 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		map = new char[N][N];
+		char [][] map = new char[N][N];
+		char [][] rMap = new char[N][N];	// 적록색약 전용
 		visited = new boolean[N][N];
-		rgVisited = new boolean[N][N];
 		
 		for(int i = 0; i < N; i++) {
-			map[i] = br.readLine().toCharArray();
+			String str = br.readLine();
+			for(int j = 0; j < N; j++) {
+				map[i][j] = str.charAt(j);
+				rMap[i][j] = map[i][j];
+				if(map[i][j] == 'G') rMap[i][j] = 'R';	// G -> R 변환
+			}
 		}
 		
 		// 일반 구역
@@ -26,35 +29,35 @@ public class Main {
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
 				if(!visited[i][j]) {
-					bfs(i, j);
+					bfs(i, j, map);
 					normalCount++;
 				}
 			}
 		}
 		
 		// 적록색약 구역
-		int rgCount = 0;
+		visited = new boolean[N][N];
+		int rCount = 0;
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				if(!rgVisited[i][j]) {
-					rgBfs(i, j);
-					rgCount++;
+				if(!visited[i][j]) {
+					bfs(i, j, rMap);
+					rCount++;
 				}
 			}
 		}
-		
-		
+			
 		// 구현 및 출력
-		System.out.println(normalCount+" "+rgCount);
+		System.out.println(normalCount+" "+rCount);
 		
 	}
 	
 	// 일반 : 현재 위치 사방탐색해서 같은 색 위치 인접영역 표시하기
-	static void bfs(int x, int y) {
+	static void bfs(int x, int y, char [][] curMap) {
 		Queue<int []> que = new LinkedList<>();
 		que.add(new int [] {x, y});
 		visited[x][y] = true;	// 방문 처리
-		char targetColor = map[x][y];
+		char targetColor = curMap[x][y];
 		
 		while(!que.isEmpty()) {
 			int [] point = que.poll();
@@ -64,51 +67,12 @@ public class Main {
 				int ty = dy[i] + point[1];
 				
 				if(isRange(tx, ty) && !visited[tx][ty]) {
-					if(targetColor == map[tx][ty]) {
+					if(targetColor == curMap[tx][ty]) {
 						visited[tx][ty] = true;
 						que.add(new int [] {tx, ty});
 					}
 				}
-				
 			}
-			
-		}
-		
-	}
-	
-	
-	
-	// 적록색약 bfs (빨강 == 파랑)
-	static void rgBfs(int x, int y) {
-		Queue<int []> que = new LinkedList<>();
-		que.add(new int [] {x, y});
-		rgVisited[x][y] = true;	// 방문 처리
-		char targetColor = map[x][y];
-		
-		while(!que.isEmpty()) {
-			int [] point = que.poll();
-			
-			for(int i = 0; i < 4; i++) {
-				int tx = dx[i] + point[0];
-				int ty = dy[i] + point[1];
-				
-				if(isRange(tx, ty) && !rgVisited[tx][ty]) {
-					if(targetColor != 'B') {	// 베이스: R || G
-						if(map[tx][ty] != 'B') { 	// 현재위치 : 적록색약 구역
-							rgVisited[tx][ty] = true;
-							que.add(new int [] {tx, ty});
-						}
-					}else {		// 베이스: B
-						if(targetColor == map[tx][ty]) {
-							rgVisited[tx][ty] = true;
-							que.add(new int [] {tx, ty});
-						}
-					}
-					
-				}
-				
-			}
-			
 		}
 	}
 	

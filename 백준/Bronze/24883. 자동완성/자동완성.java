8e029ba1e5409f63ce732cdc70dd837895178451
skyungg <167261107+skyungg@users.noreply.github.com>
2026-02-16 22:51:47 +0900

    [Bronze V] Title: 자동완성, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if(str.equals("N") || str.equals("n")) System.out.println("Naver D2");
+        else System.out.println("Naver Whale");
+    }
+}

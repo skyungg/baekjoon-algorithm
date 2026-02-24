6776b74022f07014e1bfa3c344302a63d2356adc
skyungg <167261107+skyungg@users.noreply.github.com>
2026-02-24 17:55:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 256 ms, Memory: 25588 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -16,17 +16,20 @@ public class Main {
 			}
 		}
 		
-		int result = map[0][0];
 		for(int i = 1; i < N; i++) {
 			for(int j = 0; j < i+1; j++) {
 				int left = 0;
 				int right = 0;
 				if(j-1 >= 0) left = map[i-1][j-1];
 				if(j <= i) right = map[i-1][j];
-				map[i][j] += Math.max(left, right);
 				
-				result = Math.max(result, map[i][j]);
+				map[i][j] += Math.max(left, right);
 			}
+		}
+		
+		int result = 0;
+		for(int i = 0; i < N; i++) {
+			result = Math.max(map[N-1][i], result);
 		}
 		
 		// 정답

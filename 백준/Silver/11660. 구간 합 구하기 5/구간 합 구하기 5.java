f73a22add8d370d373c1319c25b61ad01fd4c426
skyungg <167261107+skyungg@users.noreply.github.com>
2026-02-25 13:50:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1012 ms, Memory: 121392 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -15,8 +15,9 @@ public class Main {
 		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 1; j <= N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken()) + map[i][j-1];	// 누적합
-			}
+				int tmp = Integer.parseInt(st.nextToken());
+				map[i][j] =  tmp + map[i-1][j] + map[i][j-1] - map[i-1][j-1];	// (1, 1) - (i, j)까지의 사각형 합
+				}
 		}
 		
 		StringBuilder sb = new StringBuilder();
@@ -29,7 +30,7 @@ public class Main {
 			
 			int sum = 0;
 			for(int i = x1; i <= x2; i++) {
-				sum += map[i][y2] - map[i][y1 - 1];			
+				sum = map[x2][y2] - map[x2][y1-1] - map[x1-1][y2] + map[x1-1][y1-1];	
 			}
 			
 			sb.append(sum+"\n");

452df12c7dcb504a19405ba4811b42bf1c5c66f2
skyungg <167261107+skyungg@users.noreply.github.com>
2026-03-06 22:30:20 +0900

    [Silver II] Title: N과 M (9), Time: 240 ms, Memory: 28336 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 	static int N, M;
 	static int [] arr;
 	static int [] nums;
-	static int [] count;
-	static List<Integer> list = new ArrayList<>();
+	static boolean [] visited;
 	static StringBuilder sb;
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
@@ -17,29 +17,29 @@ public class Main {
 		
 		nums = new int[N];
 		arr = new int[M];
-		count = new int[10001];
+		visited = new boolean[N];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
-			count[nums[i]]++;
-			if(!list.contains(nums[i])) list.add(nums[i]);
 		}
 		
 		// 정렬
 		Arrays.sort(nums);
-		Collections.sort(list);
 		
 		// 재귀
 		sb = new StringBuilder();
-		recursion(0, 0);
+		recursion(0);
 		
 		// 출력
 		System.out.println(sb);
 		
 	}
 	
-	static void recursion(int depth, int idx) {
+	/*
+	 * point: 같은 depth에서 같은 숫자 건너뛰기
+	 * */
+	static void recursion(int depth) {
 		if(depth == M) {
 			for(int n : arr) {
 				sb.append(n+" ");
@@ -49,13 +49,16 @@ public class Main {
 			return;
 		}
 		
-		for(int i = 0; i < list.size(); i++) {
-			if(count[list.get(i)] > 0) {
-				arr[depth] = list.get(i);
-				count[list.get(i)]--;
-				recursion(depth+1, idx);
-				count[list.get(i)]++;
-			}
+		int preNum = -1;	// 이전 순서 값
+		for(int i = 0; i < N; i++) {
+			if(visited[i]) continue;	// 이미 사용 완료
+			if(nums[i] == preNum) continue;	// 같은값 반복
+			
+			visited[i] = true;
+			arr[depth] = nums[i];
+			preNum = arr[depth];
+			recursion(depth+1);
+			visited[i] = false;
 		}
 	}
 

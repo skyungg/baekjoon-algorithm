761184e27b6b4e8ac2e6f46d6e9d3445c4042268
skyungg <167261107+skyungg@users.noreply.github.com>
2026-03-20 11:42:16 +0900

    [Gold IV] Title: 알파벳, Time: 928 ms, Memory: 15980 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -4,17 +4,19 @@ import java.util.*;
 public class Main {
 	static int R, C;
 	static int [][] map;
+	static boolean [] visited = new boolean[26];	// 알파벳 방문 여부
+	static int [][] mask;
 	static int [] dx = {-1, 0, 1, 0};
 	static int [] dy = {0, 1, 0, -1};
-	static boolean [] visited = new boolean[26];	// 알파벳 방문 여부
-	static int result = 0;
  	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
+		
 		map = new int[R][C];
+		mask = new int[R][C];
 		
 		for(int r = 0; r < R; r++) {
 			String str = br.readLine();
@@ -25,14 +27,14 @@ public class Main {
 		
 		// 구현 및 정답 출력
 		visited[map[0][0]] = true;	// 출발 위치 
-		dfs(0, 0, 1);	// 자기 자신
+		mask[0][0] = 1 << map[0][0];	// 현재 위치 비트마스크
 		
-		System.out.println(result);
+		System.out.println(dfs(0, 0));
 
 	}
 	
-	static void dfs(int x, int y, int length) {
-		result = Math.max(result, length);
+	static int dfs(int x, int y) {
+		int result = 0;
 		
 		for(int i = 0; i < 4; i++) {
 			int tx = x + dx[i];
@@ -41,10 +43,17 @@ public class Main {
 			if(tx < 0 || tx >= R || ty < 0 || ty >= C) continue;
 			if(visited[map[tx][ty]]) continue;	// 이미 방문
 			
+			int num = 1 << map[tx][ty];
+			if(mask[tx][ty] == (mask[x][y] | num)) continue;
+			
+			mask[tx][ty] = mask[tx][ty] | num;
+			
 			visited[map[tx][ty]] = true;
-			dfs(tx, ty, length+1);
+			result = Math.max(result, dfs(tx, ty));
 			visited[map[tx][ty]] = false;
 		}
+		
+		return result + 1;
 	}
 
 }

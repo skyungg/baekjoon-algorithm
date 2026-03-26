e565c64078690493e96934b2a66e07e6f92fcf34
skyungg <167261107+skyungg@users.noreply.github.com>
2026-03-26 22:23:23 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 488 ms, Memory: 22212 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -28,12 +28,13 @@ public class Main {
 		
 		int idx = start + 1;
 		
-		while(idx <= end && list.get(idx) < root) {
+		while(idx <= end && list.get(idx) < root) {		// 현재 root 기준 왼쪽 트리의 마지막 인덱스 찾기
 			idx++;
 		}
 		
-		solve(start + 1, idx - 1);	// 왼쪽
-		solve(idx, end);	// 오른쪽
+		// 후위 순회
+		solve(start + 1, idx - 1);	// root 기준 왼쪽 트리 (root보다작은값)
+		solve(idx, end);	// root 기준 오른쪽 트리 (root보다작은값)
 		
 		sb.append(root+"\n");
 

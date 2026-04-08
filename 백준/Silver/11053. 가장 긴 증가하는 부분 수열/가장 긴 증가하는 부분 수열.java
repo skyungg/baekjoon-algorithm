abf6c10060359700c310eb03bef5bbeb955e5028
skyungg <167261107+skyungg@users.noreply.github.com>
2026-04-08 14:33:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,26 +7,30 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int [] A = new int[N];
+		int [] nums = new int[N];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-			
-		int [] dp = new int[N];	// dp[i]: i번째 수를 마지막으로 했을 때, 최장 길이
-		for(int i = 0; i < N; i++) dp[i] = 1;	// 적어도 자기자신 1개는 보장
 		
-		int result = 1;
-		for(int i = 1; i < N; i++) {
-			for(int j = i-1; j >= 0; j--) {
-				if(A[i] > A [j]) {
+		int [] dp = new int[N];		//dp[i] : i를 마지막 숫자로 했을때, 가장 긴 증가 수열의 값
+		
+		for(int i = 0; i < N; i++) {
+			dp[i] = 1;	// 자기자신
+			for(int j = 0; j < i; j++) {
+				if(nums[j] < nums[i]) {
 					dp[i] = Math.max(dp[i], dp[j]+1);
 				}
 			}
-			result = Math.max(dp[i], result);
 		}
 		
-		//출력
+		// 정답 출력
+		int result = 0;
+		for(int i = 0; i < N; i++) {
+			result = Math.max(result, dp[i]);
+		}
+		
 		System.out.println(result);
 
 	}

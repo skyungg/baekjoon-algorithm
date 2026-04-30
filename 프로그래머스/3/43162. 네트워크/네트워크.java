fb2af37181ef67101e51ac12785af92e94e91145
skyungg <167261107+skyungg@users.noreply.github.com>
2026-04-30 10:01:26 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,45 +1,34 @@
 /*
 -> 영역 구하기
-1. 1~n까지 bfs 돌려서 연결연결 visited 배열 표시하기
+1. 1~n까지 dfs 돌려서 연결연결 visited 배열 표시하기
 2. 1의 과정거치면서 방문하지 않은 경우-> 네트워크 1증가
 */
 
 import java.util.*;
 
 class Solution {
-    int n;
-    int [][] computers;
     boolean [] visited;
-   
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        this.n = n;
-        this.computers = computers;
         visited = new boolean[n];
         
         for(int i = 0; i< n; i++){
             if(!visited[i]){
-                bfs(i);
+                visited[i] = true;
+                dfs(i, n, computers);
                 answer++;
             }
         }
         return answer;
     }
     
-    void bfs(int start){
-        visited[start] = true;
-        Queue<Integer> que = new LinkedList<>();
-        que.add(start);
+    void dfs(int start, int n, int [][] computers){
         
-        while(!que.isEmpty()){
-            int cur = que.poll();
-            
-            for(int j = 0; j < n; j++){
-                if(cur != j){   // 자기자신 제외
-                    if(computers[cur][j] == 1 && !visited[j]){
-                        visited[j] = true;
-                        que.add(j);
-                    }
+        for(int j = 0; j < n; j++){
+            if(start != j){   // 자기자신 제외
+                if(computers[start][j] == 1 && !visited[j]){
+                    visited[j] = true;
+                    dfs(j, n, computers);
                 }
             }
         }

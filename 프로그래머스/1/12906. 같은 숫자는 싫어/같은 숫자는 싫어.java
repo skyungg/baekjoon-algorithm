f0b091a146cc4ea4bd4c6d58200c71c0db6dc56a
skyungg <167261107+skyungg@users.noreply.github.com>
2026-05-06 17:00:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.94 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,34 +1,23 @@
-/*
-배열의 순서 유지 필요
-1. STACK 써서, 마지막에 뒤집기
-2. QUE 써서 그대로 사용하기
-
-*/
 import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        // 1. STACK 쓰기
-        Stack<Integer> stack = new Stack<>();
-        for(int a : arr){
-            stack.push(a);
-        }
-        
         List<Integer> list = new ArrayList<>();
-
-        while(!stack.isEmpty()){
-            int num = stack.pop();
-            if(list.isEmpty()) list.add(num);
-            else{
-                if(list.get(list.size()-1) != num) list.add(num);
+        list.add(arr[0]);   // 초기값 삽입
+        int preNum = arr[0];
+        
+        for(int i = 1; i < arr.length; i++){
+            if(preNum != arr[i]){
+                list.add(arr[i]);
+                preNum = arr[i];
             }
         }
         
         // 배열로 만들기
         int len = list.size();
         int [] answer = new int[len];
-        for(int i = len-1; i >= 0; i--){
-            answer[len-i-1] = list.get(i);
+        for(int i = 0; i < len; i++){
+            answer[i] = list.get(i);
         }
 
         return answer;

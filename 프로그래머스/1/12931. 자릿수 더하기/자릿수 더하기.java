c35086a7bcc0e891422966fa06fe66d520b983ac
skyungg <167261107+skyungg@users.noreply.github.com>
2026-05-10 00:24:38 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,9 +4,9 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        String str = Integer.toString(n);
-        for(int i = 0; i < str.length(); i++){
-            answer +=  str.charAt(i) - '0';
+        while(n != 0){
+            answer += n % 10;   // 낮은 자리부터 더하기 시작
+            n /= 10;
         }
 
         return answer;

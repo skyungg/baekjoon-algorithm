c9af63b7085875893893e9173717f9211d6fc908
skyungg <167261107+skyungg@users.noreply.github.com>
2026-05-24 23:01:07 +0900

    [level 1] Title: 행렬의 덧셈, Time: 6.05 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,20 @@
+/*
+[[a, b], [c, d]], [[1,2],[3,4]]
+
+[[a+1, b+2], [c+3, d+4]]
+*/
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int R = arr1.length;
+        int C = arr1[0].length;
+        int[][] answer = new int[R][C];
+        
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        
+        return answer;
+    }
+}

b75ebabdb4e5db2435eb39106348fb3e81030c9f
skyungg <167261107+skyungg@users.noreply.github.com>
2026-06-02 23:34:03 +0900

    [level 2] Title: 올바른 괄호, Time: 2.47 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,21 +3,18 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
-        Stack<Character> stack = new Stack<>();
+        
+        int count = 0;
         
         for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '(') {
-                stack.add('(');
-            }
-            else{
-                if(stack.isEmpty()){
-                    return false;
-                }else stack.pop();
-            }
+            if(s.charAt(i) == '(') count++;
+            else count--;
+            
+            if(count < 0) break;
         }
         
-        if(stack.size() != 0) answer = false;
+        if(count != 0) answer = false;
+        
         return answer;
     }
 }

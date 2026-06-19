179c3637f278d200adde437a5abe3a558635d7ba
skyungg <167261107+skyungg@users.noreply.github.com>
2026-06-19 23:38:41 +0900

    [level 2] Title: 주식가격, Time: 24.94 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,14 +5,22 @@ class Solution {
         int len = prices.length;
         
         int[] answer = new int[len];
+        Stack<Integer> stack = new Stack<>();   // 주식가격이 떨어지지 않은 인덱스만 저장
         
         for(int i = 0; i < len; i++){
-            for(int j = i+1; j < len; j++){
-                answer[i]++;
-                if(prices[i] > prices[j]){
-                    break;
-                }
+            // stack이 비어있지 않고, 현재 값이 이전값보다 작을 경우-> 이전 주식가격이 떨어진 경우
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){    
+                int preIdx = stack.pop();
+                answer[preIdx] = i - preIdx;
             }
+            
+            stack.push(i);  // 현재 인덱스 삽입
+        }
+        
+        // 나머지 털기
+        while(!stack.isEmpty()){
+            int idx = stack.pop();
+            answer[idx] = len-1-idx;
         }
             
             

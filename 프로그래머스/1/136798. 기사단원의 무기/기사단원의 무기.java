00eda4d6ddb0f000a427c76ee9993f3cde6368cf
skyungg <167261107+skyungg@users.noreply.github.com>
2026-08-07 10:47:02 +0900

    [level 1] Title: 기사단원의 무기, Time: 43.86 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int cnt = 0;
+        
+        for(int i=1; i<=number; i++){
+            for(int j=1; j*j<=i; j++){
+                if(j * j == i) cnt++;
+                else if(i % j == 0) cnt+=2;
+            }
+            if (cnt > limit) cnt = power;
+            answer += cnt;
+            cnt = 0;
+        }
+        
+        return answer;
+    }
+}

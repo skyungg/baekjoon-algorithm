88617aa6c53f7ebe5f26496ee547deb7890173ba
skyungg <167261107+skyungg@users.noreply.github.com>
2026-09-11 01:02:46 +0900

    [level 3] Title: 네트워크, Time: 0.66 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 86.2 MB, 시간: 0.26 ms
+메모리: 89.2 MB, 시간: 0.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 30일 10:02:25
+2026년 09월 11일 01:02:44
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,36 @@
-/*
--> 영역 구하기
-1. 1~n까지 dfs 돌려서 연결연결 visited 배열 표시하기
-2. 1의 과정거치면서 방문하지 않은 경우-> 네트워크 1증가
-*/
-
 import java.util.*;
 
 class Solution {
-    boolean [] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n];
         
-        for(int i = 0; i< n; i++){
-            if(!visited[i]){
-                dfs(i, n, computers);
+        boolean [] visited = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){        // 그 누구와도 이어지지 않았음.
                 answer++;
+                bfs(i, computers, visited);
             }
         }
+
         return answer;
     }
     
-    void dfs(int start, int n, int [][] computers){
+    public void bfs(int start, int [][] computers, boolean [] visited){
+        Queue<Integer> que = new LinkedList<>();
+        
+        que.add(start);
         visited[start] = true;
         
-        for(int j = 0; j < n; j++){
-            if(computers[start][j] == 1 && !visited[j]){
-                dfs(j, n, computers);
+        while(!que.isEmpty()){
+            int num = que.poll();
+            
+            for(int i = 0; i < computers[num].length; i++){
+                if(computers[num][i] == 1 & !visited[i]){
+                    visited[i] = true;
+                    que.add(i);
+                }
             }
+        
         }
     }
 }

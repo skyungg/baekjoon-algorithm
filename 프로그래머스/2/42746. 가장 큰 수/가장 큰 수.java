d55ed7d70d531b53cfeb878103381c6de7e78400
skyungg <167261107+skyungg@users.noreply.github.com>
2026-09-18 19:48:01 +0900

    [level 2] Title: 가장 큰 수, Time: 103.53 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,25 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
+        int len = numbers.length;
         
-        String [] arr = new String[numbers.length];
-        
+        // 1. String 배열로 변환하기
+        String [] arr = new String[len];
         for(int i = 0; i < numbers.length; i++){
-            arr[i] = String.valueOf(numbers[i]);     // 문자열로 변환
-        }
-
-        // 정렬
-        Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
-        
-        // 맨 앞 0 체크
-        if (arr[0].equals("0")) {
-            return "0";
+            arr[i] = String.valueOf(numbers[i]);
         }
         
+        // 2. 정렬하기
+        Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));   // 큰 값대로
+        
+        // 예외 -> 맨 앞이 0인 경우
+        if(arr[0] .equals("0")) return "0";
+        
+        // 3. 정답 만들기
         StringBuilder sb = new StringBuilder();
-        for(String ar : arr) sb.append(ar);
+        for(String str : arr) sb.append(str);
         
         answer = sb.toString();
+    
         return answer;
     }
 }
